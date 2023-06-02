693a90ef736b60ab3d84f0399e17813f946596b9
Alejandro Pezuk <alejandropezuk@gmail.com>
2023-06-02 20:37:05 -0300

    Correcciones

--- src/guia1_java/ejercicio9.java
+++ src/guia1_java/ejercicio9.java
@@ -7,14 +7,14 @@ public class ejercicio9 {
         Scanner scn = new Scanner(System.in);
         
         /*
-        A partir de una cantidad de pesos que el usuario ingresa a través del
-        teclado se debe obtener su equivalente en dólares, en euros, en
-        guaraníes y en reales. Para este ejercicio se consideran las siguientes
+        A partir de una cantidad de pesos que el usuario ingresa a travï¿½s del
+        teclado se debe obtener su equivalente en dï¿½lares, en euros, en
+        guaranï¿½es y en reales. Para este ejercicio se consideran las siguientes
         tasas: 
         
-        1 dólar = 231,68 pesos
+        1 dï¿½lar = 231,68 pesos
         1 euro = 250,69 pesos
-        1 peso = 31,00 guaraníes
+        1 peso = 31,00 guaranï¿½es
         1 real = 46,81 pesos
         
         */
@@ -25,18 +25,22 @@ public class ejercicio9 {
         
         //verifico que ingrese solo numeros y en un formato esperado
         while (!spesos.matches("\\d{1,}\\.\\d{2}")) {            
-            System.out.println("Ingrese una cantidad de pesos Válido (ejemplo: 24.77 - 250.00 - 183.47) ");
+            System.out.println("Ingrese una cantidad de pesos Vï¿½lido (ejemplo: 24.77 - 250.00 - 183.47) ");
             spesos = scn.nextLine();
         }
         
         double pesos_ingresados = Double.parseDouble(spesos);
+
+        /* -------------------------------
+        La conversiÃ³n a guaranÃ­es estÃ¡ mal
+        ------------------------------- */
         double dolar = (pesos_ingresados / 231.68);
         double euro = (pesos_ingresados / 250.69);
         double guaranies = (pesos_ingresados / 31.00);
         double reales = (pesos_ingresados / 46.81);
         System.out.println("Equivalente en Dolares: " + String.format("%.2f",dolar));
         System.out.println("Equivalente en Euros: " + String.format("%.2f",euro));
-        System.out.println("Equivalente en Guaraníes: " + String.format("%.2f",guaranies));
+        System.out.println("Equivalente en Guaranï¿½es: " + String.format("%.2f",guaranies));
         System.out.println("Equivalente en Reales: " + String.format("%.2f",reales));
     }
 }

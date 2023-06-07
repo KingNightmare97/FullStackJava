866efaf3fa1cab1c969680e18be193bd3fe5aea6
KingNightmare97 <manuel.brinocoli.10@gmail.com>
2023-06-07 18:45:12 -0300

    correcciones hechas

--- src/guia1_java/ejercicio9.java
+++ src/guia1_java/ejercicio9.java
@@ -7,12 +7,12 @@ public class ejercicio9 {
         Scanner scn = new Scanner(System.in);
         
         /*
-        A partir de una cantidad de pesos que el usuario ingresa a trav�s del
-        teclado se debe obtener su equivalente en d�lares, en euros, en
-        guaran�es y en reales. Para este ejercicio se consideran las siguientes
+        A partir de una cantidad de pesos que el usuario ingresa a traves del
+        teclado se debe obtener su equivalente en dolares, en euros, en
+        guaranies y en reales. Para este ejercicio se consideran las siguientes
         tasas: 
         
-        1 d�lar = 231,68 pesos
+        1 dolar = 231,68 pesos
         1 euro = 250,69 pesos
         1 peso = 31,00 guaran�es
         1 real = 46,81 pesos
@@ -33,10 +33,14 @@ public class ejercicio9 {
 
         /* -------------------------------
         La conversión a guaraníes está mal
+        
+        ----------------------------------
+        
+        ? Correccion hecha.
         ------------------------------- */
         double dolar = (pesos_ingresados / 231.68);
         double euro = (pesos_ingresados / 250.69);
-        double guaranies = (pesos_ingresados / 31.00);
+        double guaranies = (pesos_ingresados * 31.00);
         double reales = (pesos_ingresados / 46.81);
         System.out.println("Equivalente en Dolares: " + String.format("%.2f",dolar));
         System.out.println("Equivalente en Euros: " + String.format("%.2f",euro));
